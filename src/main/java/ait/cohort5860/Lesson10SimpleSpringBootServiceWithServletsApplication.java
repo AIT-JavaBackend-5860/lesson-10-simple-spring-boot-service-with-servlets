ab83549cc0da675921c2b7ab9004f7b237a57f4c
Romyleads <romyleads@gmail.com>
2025-06-23 04:47:05 +0100

    Initial commit with Spring Boot project

---
+++ src/main/java/ait/cohort5860/Lesson10SimpleSpringBootServiceWithServletsApplication.java
@@ -0,0 +1,13 @@
+package ait.cohort5860;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson10SimpleSpringBootServiceWithServletsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson10SimpleSpringBootServiceWithServletsApplication.class, args);
+    }
+
+}
